85d0637361ded90d51ca63f07242e6c752998969
James Cote <3276350+Coteh@users.noreply.github.com>
2024-08-02 13:00:56 -0400

    Add gesture tests (currently don't work, TODOs added for later)

--- cypress/e2e/game/gameplay.cy.ts
+++ cypress/e2e/game/gameplay.cy.ts
@@ -35,9 +35,94 @@ describe("gameplay", () => {
         });
     });
 
-    // TODO: Implement this test
-    it.skip("should allow player to swipe to move the blocks", () => {
-        throw new Error("Test not implemented");
+    // TODO: In theory, this should work. But it doesn't for some reason.
+    it.skip("should allow player to swipe right to move the blocks", () => {
+        cy.viewport("iphone-xr");
+
+        cy.verifyBoard([
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 4, 0],
+            [0, 0, 0, 0],
+        ]);
+
+        cy.get("#swipeArea").realSwipe("toRight");
+
+        cy.verifyBoard([
+            [undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, 2],
+            [undefined, undefined, undefined, 4],
+            [undefined, undefined, undefined, undefined],
+        ]);
+    });
+
+    // TODO: In theory, this should work. But it doesn't for some reason.
+    it.skip("should allow player to swipe left to move the blocks", () => {
+        cy.viewport("iphone-xr");
+
+        cy.verifyBoard([
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 4, 0],
+            [0, 0, 0, 0],
+        ]);
+
+        cy.get("#swipeArea").realSwipe("toLeft");
+
+        // Alternative way of doing it (doesn't work either)
+        // cy.get("#swipeArea")
+        //     .trigger("touchstart", { touches: [{ pageX: 300, pageY: 200 }] })
+        //     .trigger("touchmove", { touches: [{ pageX: 100, pageY: 200 }] })
+        //     .trigger("touchend", { force: true });
+
+        cy.verifyBoard([
+            [undefined, undefined, undefined, undefined],
+            [2, undefined, undefined, undefined],
+            [4, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined],
+        ]);
+    });
+
+    // TODO: In theory, this should work. But it doesn't for some reason.
+    it.skip("should allow player to swipe down to move the blocks", () => {
+        cy.viewport("iphone-xr");
+
+        cy.verifyBoard([
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 4, 0],
+            [0, 0, 0, 0],
+        ]);
+
+        cy.get("#swipeArea").realSwipe("toBottom");
+
+        cy.verifyBoard([
+            [undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined],
+            [undefined, 2, 4, undefined],
+        ]);
+    });
+
+    // TODO: In theory, this should work. But it doesn't for some reason.
+    it.skip("should allow player to swipe up to move the blocks", () => {
+        cy.viewport("iphone-xr");
+
+        cy.verifyBoard([
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 4, 0],
+            [0, 0, 0, 0],
+        ]);
+
+        cy.get("#swipeArea").realSwipe("toTop");
+
+        cy.verifyBoard([
+            [undefined, 2, 4, undefined],
+            [undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined],
+        ]);
     });
 
     it("should allow player to move blocks using right arrow key", () => {
