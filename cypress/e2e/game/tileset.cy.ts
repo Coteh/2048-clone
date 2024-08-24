d114a204f7b83cf7530e6577187edf554a0c2939
James Cote <3276350+Coteh@users.noreply.github.com>
2024-08-24 14:10:04 -0400

    Implement tileset tests

--- cypress/e2e/game/tileset.cy.ts
+++ cypress/e2e/game/tileset.cy.ts
@@ -3,8 +3,7 @@
 import { GamePersistentState, GameState } from "../../../src/game";
 import { Preferences } from "../../../src/preferences";
 
-// TODO: Implement tests
-describe.skip("tileset", () => {
+describe("tileset", () => {
     beforeEach(() => {
         cy.clearBrowserCache();
         cy.visit("/", {
@@ -28,7 +27,7 @@ describe.skip("tileset", () => {
                     hasPlayedBefore: true,
                 };
                 const preferences: Preferences = {
-                    theme: "standard",
+                    theme: "classic",
                 };
                 window.localStorage.setItem("game-state", JSON.stringify(gameState));
                 window.localStorage.setItem("persistent-state", JSON.stringify(persistentState));
@@ -42,43 +41,43 @@ describe.skip("tileset", () => {
 
         cy.get("body").should(($el) => {
             const classList = $el[0].classList;
-            expect(classList.length).to.equal(1);
-            expect(classList.contains("tileset-standard")).to.be.true;
+            expect(classList.length).to.equal(2);
+            expect(classList.contains("classic")).to.be.true;
+            expect(classList.contains("tileset-modern")).to.be.true;
         });
-
-        cy.get(".setting.theme-switch").click();
-
-        cy.get("body").should("have.class", "light");
-        cy.get("body").should("have.class", "tileset-light");
-
-        cy.get(".setting.theme-switch").click();
-
-        cy.get("body").should("have.class", "dark");
-        cy.get("body").should("have.class", "tileset-dark");
-
-        cy.get(".setting.theme-switch").click();
-
+        cy.get(".settings-item").contains("Theme").should("be.visible");
+        cy.get(".settings-item").contains("Tileset").should("be.visible");
+        
+        cy.get(".setting.tileset-switch").click();
+        
+        cy.get("body").should("have.class", "classic");
+        cy.get("body").should("have.class", "tileset-classic");
+        cy.get(".settings-item").contains("Theme").should("be.visible");
+        cy.get(".settings-item").contains("Tileset").should("be.visible");
+        
+        cy.get(".setting.tileset-switch").click();
+        
+        cy.get("body").should("have.class", "classic");
+        cy.get("body").should("have.class", "tileset-colorful");
+        cy.get(".settings-item").contains("Theme").should("be.visible");
+        cy.get(".settings-item").contains("Tileset").should("be.visible");
+        
+        cy.get(".setting.tileset-switch").click();
+        
         cy.get("body").should("have.class", "classic");
         cy.get("body").should("have.class", "tileset-modern");
-
-        cy.get(".setting.theme-switch").click();
-
-        cy.get("body").should(($el) => {
-            const classList = $el[0].classList;
-            expect(classList.length).to.equal(1);
-            expect(classList.contains("tileset-standard")).to.be.true;
-        });
-
-        throw new Error("TODO: Implement test");
+        cy.get(".settings-item").contains("Theme").should("be.visible");
+        cy.get(".settings-item").contains("Tileset").should("be.visible");
     });
 
     it("should set the tileset on page reload if it's enabled in local storage for the selected theme", () => {
-        cy.get("body").should("not.have.class", "");
-
         window.localStorage.setItem(
             "preferences",
             JSON.stringify({
-                theme: "standard",
+                theme: "classic",
+                tileset: {
+                    "classic": "colorful",
+                },
             })
         );
 
@@ -86,22 +85,91 @@ describe.skip("tileset", () => {
 
         cy.get("body").should(($el) => {
             const classList = $el[0].classList;
-            expect(classList.length).to.equal(1);
-            expect(classList.contains("tileset-standard")).to.be.true;
+            expect(classList.length).to.equal(2);
+            expect(classList.contains("classic")).to.be.true;
+            expect(classList.contains("tileset-colorful")).to.be.true;
         });
-
-        throw new Error("TODO: Implement test");
     });
 
     it("should default to the first tileset for a theme if there's no selected tileset in local storage", () => {
-        throw new Error("TODO: Implement test");
+        window.localStorage.setItem(
+            "preferences",
+            JSON.stringify({
+                theme: "classic",
+            })
+        );
+
+        cy.reload();
+
+        cy.get("body").should(($el) => {
+            const classList = $el[0].classList;
+            expect(classList.length).to.equal(2);
+            expect(classList.contains("classic")).to.be.true;
+            expect(classList.contains("tileset-modern")).to.be.true;
+        });
+    });
+
+    it("should default to the first tileset for a theme if the tileset in local storage for the selected theme is invalid", () => {
+        window.localStorage.setItem(
+            "preferences",
+            JSON.stringify({
+                theme: "light",
+                tileset: {
+                    "light": "invalid",
+                },
+            })
+        );
+
+        cy.reload();
+
+        cy.get("body").should(($el) => {
+            const classList = $el[0].classList;
+            expect(classList.length).to.equal(2);
+            expect(classList.contains("light")).to.be.true;
+            expect(classList.contains("tileset-light")).to.be.true;
+        });
     });
 
     it("should default to the first tileset for a theme if tileset is set to invalid value in local storage", () => {
-        throw new Error("TODO: Implement test");
+        window.localStorage.setItem(
+            "preferences",
+            JSON.stringify({
+                theme: "classic",
+                tileset: "invalid",
+            })
+        );
+
+        cy.reload();
+
+        cy.get("body").should(($el) => {
+            const classList = $el[0].classList;
+            expect(classList.length).to.equal(2);
+            expect(classList.contains("classic")).to.be.true;
+            expect(classList.contains("tileset-modern")).to.be.true;
+        });
     });
 
     it("should hide the tileset option if a theme has just one tileset", () => {
-        throw new Error("TODO: Implement test");
+        cy.get(".settings-link").click();
+
+        cy.get(".settings-item").contains("Theme").should("be.visible");
+        cy.get(".settings-item").contains("Tileset").should("be.visible");
+
+        window.localStorage.setItem(
+            "preferences",
+            JSON.stringify({
+                theme: "standard",
+                tileset: {
+                    "standard": "standard",
+                },
+            })
+        );
+
+        cy.reload();
+
+        cy.get(".settings-link").click();
+
+        cy.get(".settings-item").contains("Theme").should("be.visible");
+        cy.get(".settings-item").contains("Tileset").should("not.be.visible");
     });
 });
