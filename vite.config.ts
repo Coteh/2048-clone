bdcb896f5e78f02ffd6c26797b497bc910fca255
James Cote <3276350+Coteh@users.noreply.github.com>
2025-01-06 19:14:45 -0500

    Revert "Display "LOCAL" label on app icon if running from vite dev server"
    This reverts commit 60ddf0d588e577a88cb53f007d135ab69b8fe37e.

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,6 @@ export default defineConfig(({ mode }) => {
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
     const env = loadEnv(mode, process.cwd(), "");
-    // Override `DEPLOY_ENV` with `LOCAL` if in development mode (ie. `vite dev`)
-    if (mode === "development") {
-        env.DEPLOY_ENV = "LOCAL";
-    }
     return {
         assetsInclude: ["**/*.md"],
         define: {
